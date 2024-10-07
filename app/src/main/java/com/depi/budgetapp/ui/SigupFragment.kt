da499374ae7f241d4ccfb1238845cf46b84fd8ca
sameh samir <samsofasam@gmail.com>
2024-10-07 13:56:37 +0300

    firebase with viewModel

--- app/src/main/java/com/depi/budgetapp/ui/SigupFragment.kt
+++ app/src/main/java/com/depi/budgetapp/ui/SigupFragment.kt
@@ -1,16 +1,12 @@
-package com.depi.budgetapp
+package com.depi.budgetapp.ui
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.depi.budgetapp.databinding.FragmentMainBinding
+import com.depi.budgetapp.R
 import com.depi.budgetapp.databinding.FragmentSigupBinding
 
 class SigupFragment : Fragment() {
---
+++ app/src/main/java/com/depi/budgetapp/viewmodels/AuthViewModel.kt
@@ -0,0 +1,9 @@
+package com.depi.budgetapp.viewmodels
+
+import androidx.lifecycle.ViewModel
+//sameh
+class AuthViewModel:ViewModel() {
+
+
+
+}
