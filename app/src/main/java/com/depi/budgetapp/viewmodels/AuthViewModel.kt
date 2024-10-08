8d0447131aa6e77f0162950fd742b4f5ce393c9b
sameh samir <samsofasam@gmail.com>
2024-10-08 10:17:18 +0300

    auth with Email and password

--- app/src/main/java/com/depi/budgetapp/viewmodels/AuthViewModel.kt
+++ app/src/main/java/com/depi/budgetapp/viewmodels/AuthViewModel.kt
@@ -1,9 +1,37 @@
 package com.depi.budgetapp.viewmodels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-//sameh
-class AuthViewModel:ViewModel() {
+import com.depi.budgetapp.repo.AuthRepository
+import com.google.firebase.auth.FirebaseUser
 
 
+class AuthViewModel(private val authRepository: AuthRepository):ViewModel() {
+
+    private val _authState = MutableLiveData<Result<FirebaseUser?>>()
+    val authState: LiveData<Result<FirebaseUser?>> get() = _authState
+
+
+    fun login(email: String, password: String) {
+        authRepository.signInWithEmailAndPassword(email, password) { result ->
+            _authState.postValue(result)
+        }
+    }
+
+    fun register(email: String, password: String) {
+        authRepository.signUpWithEmail(email, password) { result ->
+            _authState.postValue(result)
+        }
+    }
+
+    // to make sign out from Qash
+    fun signOut() {
+        authRepository.signOut()
+    }
+
+    fun getCurrentUser(): FirebaseUser? {
+        return authRepository.getCurrentUser()
+    }
 
 }
