efb7d7429406c10447368d068580590c8c0e7a1d
sameh samir <samsofasam@gmail.com>
2024-10-08 10:17:18 +0300

    auth with Email and password

--- app/src/main/java/com/depi/budgetapp/repo/AuthRepository.kt
+++ app/src/main/java/com/depi/budgetapp/repo/AuthRepository.kt
@@ -1,6 +1,48 @@
 package com.depi.budgetapp.repo
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
 class AuthRepository {
 
+    private val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    fun signInWithEmailAndPassword(
+        email: String,
+        password: String,
+        callback: (Result<FirebaseUser?>) -> Unit
+    ) {
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    callback(Result.success(firebaseAuth.currentUser))
+                } else {
+                    callback(Result.failure(task.exception ?: Exception("Authentication failed")))
+                }
+            }
+    }
+
+    fun signUpWithEmail(
+        email: String,
+        password: String,
+        callback: (Result<FirebaseUser?>) -> Unit
+    ) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                callback(Result.success(firebaseAuth.currentUser))
+            } else {
+                callback(Result.failure(task.exception ?: Exception("Authentication failed")))
+            }
+        }
+    }
+
+    fun signOut() {
+        firebaseAuth.signOut()
+    }
+
+    fun getCurrentUser(): FirebaseUser? {
+        return firebaseAuth.currentUser
+    }
 
 }
